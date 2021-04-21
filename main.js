a11386d0d162a9309439c7b92e6e724433750641
Jonas Holbech <jh@jonasholbech.dk>
2021-04-21 09:25:29 +0200

    works

--- main.js
+++ main.js
@@ -14,14 +14,24 @@ function showProducts(products) {
   products.forEach(showProduct);
 }
 
+function getValue(id) {
+  const item = basket.find((item) => item.id === id);
+  if (!item) {
+    return 0;
+  }
+  return item.amount;
+}
 function showProduct(product) {
   const template = document.querySelector("#product").content;
   const copy = template.cloneNode(true);
   copy.querySelector("h1").textContent = product.name;
   copy.querySelector("h2").textContent = product.price;
-
+  copy.querySelector("input").value = getValue(product.id);
   copy.querySelector(".minus").addEventListener("click", () => {
     subtractOne(product.id);
+  });
+  copy.querySelector("input").addEventListener("blur", (e) => {
+    addOne(product.id, e.target.valueAsNumber);
   });
   copy.querySelector(".plus").addEventListener("click", () => {
     addOne(product.id);
@@ -39,19 +49,20 @@ function subtractOne(id) {
   basket = tempBasket.filter((item) => item.amount > 0);
   render();
 }
-function addOne(id) {
+function addOne(id, amount = 1) {
   const exists = basket.findIndex((item) => item.id === id);
   console.log(exists);
   if (exists === -1) {
-    basket.push({ ...products.find((item) => item.id === id), amount: 1 });
+    basket.push({ ...products.find((item) => item.id === id), amount: amount });
   } else {
     basket = basket.map((item) => {
       if (item.id === id) {
-        item.amount++;
+        item.amount = item.amount + amount;
       }
       return item;
     });
   }
+  basket = basket.filter((item) => item.amount > 0);
   render();
 }
 function showBasket(basket) {
